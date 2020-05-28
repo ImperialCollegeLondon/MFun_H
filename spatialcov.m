1ec789ec216e71d7779deced5b09a82844c2940c
TitianChen <yuting.chen17@imperial.ac.uk>
2020-05-28 17:02:54 +0100

    compute spatial autocovariance-final

--- spatialcov.m
+++ spatialcov.m
@@ -4,7 +4,7 @@
 % maximum distance smax
 % The outputs of the function are the spatial covariance values and the
 % covariance distance.
-function [covariance,distance,r,disI,disJ,Ind_is,Ind_js]=spatialcov(X,Y,D,ds,smax,r,disI,disJ,Ind_is,Ind_js)
+function [covariance,distance,r,disI,disJ,Ind_p1s,Ind_p2s]=spatialcov(X,Y,Data,ds,smax,r,disI,disJ,Ind_p1s,Ind_p2s)
 %
 % Note:
 % This script is designed for QUICKLY computing spatial autocovariance from a
@@ -12,60 +12,58 @@ function [covariance,distance,r,disI,disJ,Ind_is,Ind_js]=spatialcov(X,Y,D,ds,sma
 % @ Yuting
 %
 
-if length(size(D)) == 2
-    D = reshape(D,[size(D),1]);
+if length(size(Data)) == 2
+    Data = reshape(Data,[size(Data),1]);
 end
-if prod(size(D,[1,2]))>12100 || prod(size(D,[1,2,3]))>12100*20000
+if prod(size(Data,[1,2]))>12100 || prod(size(Data,[1,2,3]))>12100*20000
     error('Normal PCs Memory might be not enough to compute this BIG array in this fast-script.')
 end
 
 X = X(:);
 Y = Y(:);
-val = D(:);
+val = Data(:);
 vest=std(val);
 disp(['Mean: ',num2str(mean(val)),' Variance: ',num2str((vest))])
-distance = NaN(round(smax/ds/2),size(D,3));
-covariance = zeros(round(smax/ds)+2,size(D,3));
-ncov = zeros(round(smax/ds)+2,1);
-
+covariance = zeros(round(smax/ds)+2,size(Data,3));
+ncov = zeros(round(smax/ds)+2,size(Data,3));
+distance = [1:smax]';
 if isempty(r)
     r = zeros(length(X),length(X));
     [disI,disJ] = deal(cell(1+smax,1));
-    for i = 1:length(X)
-        for j = 1:length(X)
-            if i <= j % prevent double counting
-                r(i,j) = sqrt((X(i)-X(j)).^2+(Y(i)-Y(j)).^2);
+    for p1_i = 1:length(X)
+        for p2_i = 1:length(X)
+            if p1_i <= p2_i % prevent double counting
+                r(p1_i,p2_i) = sqrt((X(p1_i)-X(p2_i)).^2+(Y(p1_i)-Y(p2_i)).^2);
             end
         end
     end
     r = int16(round(r));
+    [Ind_p1s,Ind_p2s] =  deal([]);
+    for ir = 1:length(distance)
+        [p1_s,p2_s] = find(r==distance(ir));
+        Ind_p1s{ir,1} = int16(p1_s);
+        Ind_p2s{ir,1} = int16(p2_s);
+    end
 end
-distance = [1:smax]';
-[Ind_is,Ind_js] =  deal([]);
-for ir = 1:length(distance)
-    [is,js] = find(r==distance(ir));
-    Ind_is{ir,1} = int16(is);
-    Ind_js{ir,1} = int16(js);
-end
-D = reshape(D,[],size(D,3));
+Data = reshape(Data,[],size(Data,3));
 for ir = 1:length(distance)
     tic
-    is = Ind_is{ir};
-    js = Ind_js{ir};
-    thisWholeLen = length(is);
+    p1_s = Ind_p1s{ir};
+    p2_s = Ind_p2s{ir};
+    thisWholeLen = length(p1_s);
     setStart = 1;
     setLen = 500000;% this number is set based on yt PC's total RAM.
     for setNo = 1:ceil(thisWholeLen/setLen)
         setInd = setStart:1:min(setStart+setLen-1,thisWholeLen);
         if ~isempty(setInd)
-            covariance(ir,:) = covariance(ir,:)+sum(D(is,:) & D(js,:),1) ;
+            covariance(ir,:) = covariance(ir,:)+...
+                sum(Data(p1_s(setInd),:)&Data(p2_s(setInd),:),1);
             setStart = setInd(end)+1;
         end
     end
-    ncov(ir) = ncov(ir)+length(is);
+    ncov(ir,:) = ncov(ir,:)+thisWholeLen;
     toc
 end
-
 
 % for i = 1:size(D,1)
 %     for j = 1:size(D,1)
@@ -77,16 +75,12 @@ end
 %     end
 % end
 
-for imageNo = 1:size(D,2)
-    covariance(:,imageNo) = covariance(:,imageNo)./ncov;  
-end
+covariance = covariance./ncov;  
 covariance((length(distance)+1):end,:) = [];
 
 % figure
 plot(distance,nanmean(covariance,2),'o')
 xlabel('Distance')
 xlabel('Covariance')
-
-
 
 
