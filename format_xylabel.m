f9a789531092938988fd83fa945b6e37a0f9cfea
Yuting Chen <yuting.chen17@imperial.ac.uk>
2020-12-03 02:48:21 +0800

    remove unnecessary files

--- format_xylabel.m
+++
@@ -1,4 +0,0 @@
-function format_xylabel(ha,rowno,colno)
-set(ha(((1:rowno)-1)'*colno+(2:colno)),'YTickLabel',[],'YLabel',[])
-set(ha(1:(colno*(rowno-1))),'XTickLabel',[],'XLabel',[])
-end
--- getIDWweight.m
+++
@@ -1,9 +0,0 @@
-function weight = getIDWweight(DIST,P)
-%
-% Inverse Distance Weighting method
-
-dis = DIST(:);
-weight = ((1./(dis.^P))./nansum(1./(dis.^P)));
-weight = reshape(weight,size(DIST));
-
-end
